c0a8ad3f5e9c172de07d1e5930be2837ac2022c3
Dumont-Julien <dumontj357@gmail.com>
2024-04-01 12:40:49 +0200

    php-00 exo 02

--- php00/ex02/condition.php
+++ php00/ex02/condition.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 $nombre = mt_rand(1, 10);
-if (in_array($nombre, [1, 3, 5, 7, 9])) {
+if ($nombre % 2) {
     echo "Le nombre $nombre est impair";
 } else {
     echo "Le nombre $nombre est pair";
